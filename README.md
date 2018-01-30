6fc106865e3315d890fcafb6b08c1829598850bd
Clayton Hickey <clayton.hickey@yahoo.com>
2018-01-29 23:03:54 -0500

    Added support for other platforms to test

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # bird-camera
+
+# Example conf.py
+
+```
+is_pi = True # Ensure platform is a pi
+
+# Twitter keys
+
+# From https://apps.twitter.com/
+
+twitter = {
+    'consumer_key': '<~25 chars>',
+    'consumer_secret': '<~50 chars>',
+    'access_token': '<~18 numbers>-<~31 chars>',
+    'access_token_secret': '<~45 chars>'
+}
+
+# Firebase conf
+
+# From https://console.firebase.google.com
+
+firebase = {
+    'database_url': 'https://<firebase_project_ID>.firebaseio.com/'
+}
+```
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os.path
+
+from conf import is_pi
+
 from firebasef import rdb
 db = rdb()
 
@@ -6,24 +10,39 @@ db.update('spottings/', ({
 }))
 
 from twitter import tweet # Import basic twitter functions
-from gpiozero import MotionSensor # Import motion sensor
-from picamera import PiCamera # Import pi camera
 
-try:
-    pir = MotionSensor(4) # Instantiate motion sensor
-except gpio.exe.PinFixedPull:
-    print('Error getting MotionSensor. Do you have it plugged into GPIO4?')
+if is_pi:
+    from gpiozero import MotionSensor # Import motion sensor
+    from picamera import PiCamera # Import pi camera
 
-camera = PiCamera()
+    try:
+        pir = MotionSensor(4) # Instantiate motion sensor
+    except gpio.exe.PinFixedPull:
+        print('Error getting MotionSensor. Do you have it plugged into GPIO4?')
+
+    camera = PiCamera()
 
 def on_detection():
-    camera.capture('./img.jpg')
-    tweet('I spotted: ', './img.jpg')
+    if is_pi:
+        camera.capture('./img.jpg')
+    try:
+        tweet('I spotted: ', './img.jpg')
+        print('Tweeted ./img.jpg with the caption: \'I spotted: \'')
+    except IOError:
+        print("./img.jpg cannot be parsed/detected; does the file exist & is it a jpg?")
+
+if not is_pi:
+    print("conf.py states that this platform isn't a pi... going into testing mode.")
 
 # main run loop
 while True:
-    pir.wait_for_motion()
-    camera.start_preview()
-    on_detection()
-    pir.wait_for_no_motion()
-    camera.stop_preview()
+    if is_pi:
+        pir.wait_for_motion()
+        camera.start_preview()
+        on_detection()
+        pir.wait_for_no_motion()
+        camera.stop_preview()
+    else:
+        print('')
+        p = raw_input("Press Enter to take a fake picture from ./img.png")
+        on_detection()
