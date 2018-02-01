412529cbf4da9710bdab52465c1bd57880a00448
clay53 <diamondminer81@gmail.comgit config --global user.name clay53>
2018-02-01 16:49:30 -0500

    I think I fixed it

--- main.py
+++ main.py
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-from firebasef import rdb
-db = rdb()
-
-db.update('spottings/', ({
-    'test': 'yay'
-}))
-
-from twitter import tweet # Import basic twitter functions
-from gpiozero import MotionSensor # Import motion sensor
-from picamera import PiCamera # Import pi camera
-
-try:
-    pir = MotionSensor(4) # Instantiate motion sensor
-except gpio.exe.PinFixedPull:
-    print('Error getting MotionSensor. Do you have it plugged into GPIO4?')
-
-camera = PiCamera()
-
-def on_detection():
-    camera.capture('./img.jpg')
-    tweet('I spotted: ', './img.jpg')
-
-# main run loop
-while True:
-    pir.wait_for_motion()
-    camera.start_preview()
-    on_detection()
-    pir.wait_for_no_motion()
-    camera.stop_preview()
-=======
 import os.path
 
 from conf import is_pi
@@ -77,4 +46,3 @@ while True:
         print('')
         p = raw_input("Press Enter to take a fake picture from ./img.png")
         on_detection()
->>>>>>> e50165ac40ebc3d885aa3c27cdf9e22c3b99ba62
--- twitter.py
+++ twitter.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from twython import Twython # Import Twitter's API module
 
 # Import keys from conf.py
@@ -19,26 +18,4 @@ def tweet(msg, img):
 		response = twitter.upload_media(media=photo) # Load responses
 		twitter.update_status(status=msg, media_ids=[response['media_id']]) # Update status with response
 	else:
-=======
-from twython import Twython # Import Twitter's API module
-
-# Import keys from conf.py
-from conf import (twitter)
-
-# Start Twitter API
-twitter = Twython(
-	twitter['consumer_key'],
-	twitter['consumer_secret'],
-	twitter['access_token'],
-	twitter['access_token_secret']
-)
-
-# Basic tweet function
-def tweet(msg, img):
-	if img:
-		photo = open(img, 'rb') # Turn img dir to img variable
-		response = twitter.upload_media(media=photo) # Load responses
-		twitter.update_status(status=msg, media_ids=[response['media_id']]) # Update status with response
-	else:
->>>>>>> e50165ac40ebc3d885aa3c27cdf9e22c3b99ba62
 		twitter.update_status(status=msg)
